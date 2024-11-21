8c5de268d6906266fc5e344ad957b7454cdd0e20
Richard Brearton <richardbrearton@gmail.com>
2024-11-21 21:53:22 +0000

    Made the version number dynamic

--- src/sprongle/__init__.py
+++ src/sprongle/__init__.py
@@ -1,2 +1,6 @@
-def hello() -> str:
-    return "Hello from sprongle!"
+"""The home of the sprongle website."""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("sprongle")
+"""The version of the sprongle library, as set in the `pyproject.toml` file."""
