140265cc8323d317419d309e8cb246804667847a
Richard Brearton <richardbrearton@gmail.com>
2024-11-21 23:17:09 +0000

    Added option for header button icons

--- src/sprongle/components/_header_button.py
+++ src/sprongle/components/_header_button.py
@@ -4,8 +4,8 @@ from nicegui import ui
 
 
 class HeaderButton(ui.button):
-    def __init__(self, text: str, link: str) -> None:
-        super().__init__(text=text, on_click=lambda: ui.navigate.to(link))
+    def __init__(self, text: str, link: str, *, icon: str | None = None) -> None:
+        super().__init__(text=text, on_click=lambda: ui.navigate.to(link), icon=icon)
 
         self.classes("text-white py-2 px-4 text-lg")
         self.props("rounded flat no-caps")
