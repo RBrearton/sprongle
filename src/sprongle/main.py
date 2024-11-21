86b2d4e27d53d34f8a63014ec62c2b7f36223790
Richard Brearton <richardbrearton@gmail.com>
2024-11-21 22:14:22 +0000

    Updated the main function

--- src/sprongle/main.py
+++ src/sprongle/main.py
@@ -1,2 +1,20 @@
 """Running this file runs the website."""
 
+from nicegui import ui
+
+from .components import Page
+
+
+@ui.page("/")
+def home() -> None:
+    """Build the home page."""
+    Page(ui.markdown("Welcome to sprongle!"))
+
+
+def main() -> None:
+    """Run the website."""
+    ui.run()
+
+
+if __name__ == "__main__":
+    main()
