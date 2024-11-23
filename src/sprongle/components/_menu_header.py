11eaba3bc62e86f2ff97ef852ac95e17173e84a4
Richard Brearton <richardbrearton@gmail.com>
2024-11-23 23:55:19 +0000

    Added a MenuHeader component

---
+++ src/sprongle/components/_menu_header.py
@@ -0,0 +1,11 @@
+"""Define the MenuHeader component."""
+
+from nicegui import ui
+
+
+class MenuHeader(ui.html):
+    """The MenuHeader component is used to make headers for the menus."""
+
+    def __init__(self, text: str) -> None:
+        super().__init__(content=text, tag="q-item-label")
+        self.props("header")
