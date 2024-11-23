eaca9367ee0c280b8c8509a22bdd13629c264393
Richard Brearton <richardbrearton@gmail.com>
2024-11-23 23:55:13 +0000

    Added MenuItem component

---
+++ src/sprongle/components/_menu_item.py
@@ -0,0 +1,13 @@
+"""Define a simple menu item that links to a page."""
+
+from nicegui import ui
+
+
+class MenuItem(ui.item):
+    """A menu item that links to a page."""
+
+    def __init__(self, text: str, link: str) -> None:
+        super().__init__(text=text, on_click=lambda: ui.navigate.to(link))
+
+        self.props("flat")
+        self.classes("w-full")
