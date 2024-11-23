d6c8e4d796265c65c5419e5702f36e948432da3e
Richard Brearton <richardbrearton@gmail.com>
2024-11-23 23:55:04 +0000

    Added Menu class

---
+++ src/sprongle/components/_menu.py
@@ -0,0 +1,21 @@
+"""Define the menu component."""
+
+from nicegui import ui
+
+from ._menu_expansion_item import MenuExpansionItem
+from ._menu_header import MenuHeader
+
+
+class Menu(ui.list):
+    """The sprongle app's menu component."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.classes("w-full rounded-borders")
+        self.props("bordered clickable v-ripple")
+
+        with self:
+            MenuHeader("Physics")
+            MenuExpansionItem("B2: Symmetry and relativity")
+            MenuExpansionItem("B6: Condensed matter physics")
