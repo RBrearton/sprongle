e9a0b91247a10c1146bb52f3e5e17fafd73071d7
Richard Brearton <richardbrearton@gmail.com>
2024-11-23 23:55:44 +0000

    Updated public components

--- src/sprongle/components/__init__.py
+++ src/sprongle/components/__init__.py
@@ -4,6 +4,9 @@ from ._github_link import GithubLink
 from ._header import Header
 from ._header_button import HeaderButton
 from ._left_drawer import LeftDrawer
+from ._menu import Menu
+from ._menu_expansion_item import MenuExpansionItem
+from ._menu_item import MenuItem
 from ._right_drawer import RightDrawer
 
 __all__ = [
@@ -11,5 +14,8 @@ __all__ = [
     "Header",
     "HeaderButton",
     "LeftDrawer",
+    "Menu",
+    "MenuExpansionItem",
+    "MenuItem",
     "RightDrawer",
 ]
