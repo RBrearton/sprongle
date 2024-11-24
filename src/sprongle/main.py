424f64507cf31e5e3305ac39c25de821f062dacb
Richard Brearton <richardbrearton@gmail.com>
2024-11-24 16:28:16 +0000

    First pass at a main module with a nice scss file

--- src/sprongle/main.py
+++ src/sprongle/main.py
@@ -1,19 +1,28 @@
 """Running this file runs the website."""
 
-from nicegui import ui
+from nicegui import app, ui
 
 from . import components as c
+from .config import parsed_config as config
 
 
 def page_template() -> None:
     """Build the page template."""
+    # This is needed for the github logo.
+    eva_icons = "https://unpkg.com/eva-icons@1.1.3/style/eva-icons.css"
+    ui.add_head_html(f'<link href={eva_icons} rel="stylesheet" />')
+
+    # Add our static styles.css file. This is built automatically from our
+    # assets/styles.scss file.
     ui.add_head_html(
-        '<link href="https://unpkg.com/eva-icons@1.1.3/style/eva-icons.css" rel="stylesheet" />'  # noqa: E501
+        f'<link rel= "stylesheet" href="/{config.static_dir_name}/styles.css">'
     )
 
+    # Now build the core page components.
     c.Header(title="Sprongle")
     c.LeftDrawer()
     c.RightDrawer()
+    ui.query("q-focus-helper").classes("active:bg-amber-400")
 
 
 @ui.page("/")
@@ -21,10 +30,18 @@ def home() -> None:
     """Build the home page."""
     page_template()
     ui.markdown("Welcome to sprongle!")
+    ui.button("defaults")
 
 
 def main() -> None:
     """Run the website."""
+    # Make sure that our static directory is being served.
+    app.add_static_files(
+        url_path=f"/{config.static_dir_name}",
+        local_directory=config.static_dir_path,
+    )
+
+    # Run the website.
     ui.run()
 
 
