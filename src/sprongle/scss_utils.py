1e6eff1f8463bb60822be62130d89786fe8b5153
Richard Brearton <richardbrearton@gmail.com>
2024-11-24 18:59:17 +0000

    Added a trivial bit of logging

--- src/sprongle/scss_utils.py
+++ src/sprongle/scss_utils.py
@@ -9,3 +9,4 @@ def compile_scss() -> None:
     """Compile our scss to a static css file."""
     css = sass.compile(filename=str(config.scss_file))
     config.static_css_file.write_text(css)
+    print("Compiled scss to css.")
