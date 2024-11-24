fb18c1499bf4014c7d530bd94bef5d97b0397730
Richard Brearton <richardbrearton@gmail.com>
2024-11-24 16:01:14 +0000

    Added a simple config module

---
+++ src/sprongle/config.py
@@ -0,0 +1,24 @@
+"""Contains the definition of the app's config."""
+
+from pathlib import Path
+
+from pydantic import DirectoryPath, FilePath
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    """The app's configuration is defined by this class."""
+
+    static_dir: DirectoryPath = Path(__file__).parent.parent / "static"
+    assets_dir: DirectoryPath = Path(__file__).parent.parent / "assets"
+    scss_file: FilePath = assets_dir / "styles.scss"
+
+    model_config = SettingsConfigDict(env_prefix="sprongle_")
+
+
+parsed_config = Config()
+"""This is parsed when the app starts up.
+
+Optionally, users can also instantiate Config() manually to get an up-to-date
+config object, at the cost of a small performance hit.
+"""
