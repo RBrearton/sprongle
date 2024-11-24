0015fa468758e2b8e5fd611d90ec542e39ee4b65
Richard Brearton <richardbrearton@gmail.com>
2024-11-24 23:00:50 +0000

    Various corrections to the header button; nicegui button isn't a quasar button!

--- src/sprongle/components/_header_button.py
+++ src/sprongle/components/_header_button.py
@@ -2,10 +2,19 @@
 
 from nicegui import ui
 
+from sprongle import style
 
-class HeaderButton(ui.button):
-    def __init__(self, text: str, link: str, *, icon: str | None = None) -> None:
-        super().__init__(text=text, on_click=lambda: ui.navigate.to(link), icon=icon)
 
-        self.classes("text-white py-2 px-4 text-lg")
+class HeaderButton(ui.html):
+    def __init__(self, text: str, link: str) -> None:
+        super().__init__(content=text, tag="q-btn")
+
+        # When the button is clicked, navigate to the link.
+        self.on("click", lambda: ui.navigate.to(link))
+
+        # This is a clickable in the header, so...
+        self.classes(style.header_clickable)
+
+        # These are specific to the fact that this is a button.
         self.props("rounded flat no-caps")
+        self.classes("py-2 px-4 text-lg")
