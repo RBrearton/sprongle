d8f5a91f7056c422b2b0d6000b7f248360b516a8
Richard Brearton <richardbrearton@gmail.com>
2024-11-26 23:59:27 +0000

    Added bg-primary to the foter

--- src/sprongle/components/_footer.py
+++ src/sprongle/components/_footer.py
@@ -2,12 +2,15 @@
 
 from nicegui import ui
 
+from sprongle.style import bg_primary
+
 
 class Footer(ui.footer):
     """The sprongle app's footer component."""
 
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(fixed=False)
+        self.classes(bg_primary)
 
         with self:
             ui.markdown("© 2024 Dr. Richard Brearton")
