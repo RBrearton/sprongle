19e6cbf2e095e41f8291c39162b85c4bfa2fb77a
Richard Brearton <richardbrearton@gmail.com>
2024-11-26 23:03:00 +0000

    Loads of minor tweaks for DarkModeToggle

--- src/sprongle/components/_dark_mode_toggle.py
+++ src/sprongle/components/_dark_mode_toggle.py
@@ -3,10 +3,19 @@
 from nicegui import ui
 
 
-class DarkModeToggle(ui.switch):
+class DarkModeToggle(ui.element):
     """A toggle switch for dark mode."""
 
     def __init__(self) -> None:
         super().__init__()
 
-        self.on_value_change(ui.dark_mode().toggle)
+        self.dark_mode = ui.dark_mode()
+
+        with self.classes("flex flex-row items-center h-full"):
+            ui.icon("light_mode").classes("text-2xl")
+            self.switch = ui.switch(on_change=self.dark_mode.toggle).classes(
+                "text-secondary"
+            )
+            self.switch.props["color"] = "white"
+            self.switch.props("keep-color")
+            ui.icon("dark_mode").classes("text-2xl")
