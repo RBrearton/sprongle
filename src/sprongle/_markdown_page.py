763b54563e69f29fdf3988b9497865f83e229629
Richard Brearton <richardbrearton@gmail.com>
2024-11-27 15:02:49 +0000

    Added a simple MarkdownPage class

---
+++ src/sprongle/_markdown_page.py
@@ -0,0 +1,33 @@
+"""Define the MarkdownPage class."""
+
+from nicegui import ui
+
+from sprongle import utils
+
+from ._sprongle_page import SpronglePage
+
+
+class MarkdownPage(SpronglePage):
+    """A SpronglePage that's generated from the path to a markdown file."""
+
+    def __init__(
+        self,
+        subdomain_name: str,
+        page_path: str,
+    ) -> None:
+        super().__init__(subdomain_name)
+
+        self._page_path = page_path
+        self._parsed_markdown = utils.get_markdown(self._page_path)
+
+    def make_right_drawer(self) -> None:
+        return None
+
+    def make_right_drawer_content(self) -> None:
+        return None
+
+    def make_content(self) -> ui.element:
+        return ui.markdown(
+            self._parsed_markdown,
+            extras=["tables", "fenced-code-blocks", "latex"],
+        )
