669908d1cf40b06eacbb208825eb73e2527cd770
Richard Brearton <richardbrearton@gmail.com>
2024-11-27 11:46:35 +0000

    Allowed for more finegrained control over content placement

--- src/sprongle/main.py
+++ src/sprongle/main.py
@@ -5,11 +5,18 @@ from nicegui import app, ui
 from . import components as c
 from .config import parsed_config as config
 from .scss_utils import compile_scss
-from .style import bg_100
+from .style import bg_100, text_bg_content
 
 
-def page_template(subdomain: str) -> ui.element:
-    """Build the page template."""
+def page_template(subdomain: str) -> tuple[ui.element, ui.element]:
+    """Build the page template.
+
+    This function builds the core page components that are shared across all
+    pages on the website. It returns a tuple where the first element is a very
+    basic central div covering the entire central area, and the second element
+    is a content area that is centered and has a max width for nice content
+    display.
+    """
     # This is needed for the github logo.
     eva_icons = "https://unpkg.com/eva-icons@1.1.3/style/eva-icons.css"
     ui.add_head_html(f'<link href={eva_icons} rel="stylesheet" />')
@@ -28,16 +35,36 @@ def page_template(subdomain: str) -> ui.element:
     c.RightDrawer()
     c.Footer()
 
-    page = ui.element().classes(f"w-full h-full {bg_100}")
-    return page.classes("min-h-screen")
+    # The central div left aligns content with no padding. This is the simplest
+    # thing we could let users work with.
+    central_div = ui.element().classes(
+        f"w-full h-full {bg_100} {text_bg_content} min-h-screen"
+    )
+
+    # We also want to make a neat material-mkdocs style content area with a
+    # central alignment and a fixed max width for nice blog-style content.
+    with central_div:
+        # Make a pure alignment div.
+        with ui.element().classes(
+            "flex flex-col justify-start items-center w-full"
+        ):
+            # max-w-850px says that this should never exceed 850px.
+            # w-full encourages the div to reach exactly that width, even if the
+            # content isn't particularly wide. This keeps sizes consistent.
+            # flex-shrink makes this div shrinkable when screen space decreases.
+            content_area = ui.element().classes(
+                "max-w-[850px] w-full flex-shrink py-6"
+            )
+
+    return central_div, content_area
 
 
 @ui.page("/")
 def home() -> None:
     """Build the home page."""
-    page = page_template("/physics")
+    _, content_area = page_template("/physics")
 
-    with page:
+    with content_area:
         ui.markdown("Welcome to sprongle!")
 
 
