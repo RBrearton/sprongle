40647413df82811f3104ee6171c0071fe93b2711
Richard Brearton <richardbrearton@gmail.com>
2024-11-27 11:44:48 +0000

    Deleted now unused get_case_sensitive_path function

--- src/sprongle/utils.py
+++ src/sprongle/utils.py
@@ -1,39 +1,5 @@
 """Define some general utilities used in the sprongle app."""
 
-from pathlib import Path
-
-
-def get_case_sensitive_path(path: Path) -> Path:
-    """Given a Path object, return the case-correct version of the path."""
-
-    def get_err(path: Path) -> FileNotFoundError:
-        return FileNotFoundError(f"The path {path} does not exist.")
-
-    parts = path.parts
-    correct_parts: list[str] = []
-
-    for part in parts:
-        if not correct_parts:
-            # This is the root part (e.g., '/', 'C:\\')
-            correct_parts.append(part)
-        else:
-            # Get the directory contents
-            parent_dir = Path(*correct_parts)
-            children = [child.name for child in parent_dir.iterdir()]
-            matching_child = next(
-                (child for child in children if child.lower() == part.lower()),
-                None,
-            )
-
-            # Make sure the child exists.
-            if matching_child is None:
-                raise get_err(Path(*correct_parts, part))
-
-            # Add the child to the correct parts.
-            correct_parts.append(matching_child)
-
-    return Path(*correct_parts)
-
 
 def url_from_topic_name(topic_name: str) -> str:
     """Return the relative part of the url associated with a topic name.
