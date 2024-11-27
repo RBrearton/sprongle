baaa01bdb98aaa07b65ae0579ceb324a3e3c3310
Richard Brearton <richardbrearton@gmail.com>
2024-11-27 14:30:36 +0000

    Defined a simple PageBuilder helper class

---
+++ src/sprongle/_page_builder.py
@@ -0,0 +1,33 @@
+"""Define the PageBuilder class."""
+
+from abc import ABC, abstractmethod
+
+from nicegui import ui
+
+
+class PageBuilder(ABC):
+    """Implement PageBuilder to make your page visible on the sprongle app."""
+
+    @abstractmethod
+    def make_header(self) -> ui.element:
+        """Build the header for the page."""
+
+    @abstractmethod
+    def make_footer(self) -> ui.element:
+        """Build the footer for the page."""
+
+    @abstractmethod
+    def make_content(self) -> ui.element:
+        """Build the ui element that should live in the main content area."""
+
+    @abstractmethod
+    def make_left_drawer_content(self) -> ui.element:
+        """Build the ui element that should live in the left drawer."""
+
+    @abstractmethod
+    def make_right_drawer_content(self) -> ui.element | None:
+        """Build the ui element that should live in the right drawer.
+
+        This is an optional element; return None if you don't want a right
+        drawer.
+        """
