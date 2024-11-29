a627397e634d1224a370f4581f77c2ba59f818d5
Richard Brearton <richardbrearton@gmail.com>
2024-11-29 21:46:31 +0000

    Some bugfixes to the Text class

--- src/sprongle/components/_text.py
+++ src/sprongle/components/_text.py
@@ -38,5 +38,17 @@ class Text(ui.html):
     """
 
     def __init__(self, text: str, *, is_paragraph: bool = True) -> None:
-        super().__init__(content=text, tag="p" if is_paragraph else "span")
+        rendered_text = render_markdown(text)
+
+        # This always renders as a paragraph, which we never want, because this
+        # is going to be put as an input into either a <p> or a <span> tag. If
+        # we didn't do this and we asked for a paragraph, our html would look
+        # like <p><p>...</p></p>, which is never what we want.
+        if rendered_text.startswith("<p>"):
+            # We still check for the <p>, just in case!
+            rendered_text = rendered_text[3:-5]
+
+        super().__init__(
+            content=rendered_text, tag="p" if is_paragraph else "span"
+        )
         self.classes("w-full")
