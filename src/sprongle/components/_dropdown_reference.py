20a491ae4fbc2f3fba580e262a26978316e0b173
Richard Brearton <richardbrearton@gmail.com>
2024-12-01 10:44:46 +0000

    Loads of improvements to the dropdown reference - finally looking good

--- src/sprongle/components/_dropdown_reference.py
+++ src/sprongle/components/_dropdown_reference.py
@@ -2,12 +2,13 @@
 
 from nicegui import ui
 
+from sprongle.color import light
 from sprongle.style import bg_200
 
 from ._text import Text
 
 
-class DropdownReference(ui.dropdown_button):
+class DropdownReference(ui.element):
     """The central DropdownReference component.
 
     This is the way that we try to get around not having equation numbers.
@@ -20,16 +21,27 @@ class DropdownReference(ui.dropdown_button):
         this reference appears. The ref is the ui element to show in the
         dropdown.
         """
-        super().__init__(text)
+        super().__init__()
         self.classes("inline")
-        self.props("flat no-caps")
-        self.props("dense")
-        self.props(f"label={text}")
+        self.classes("flex flex-col")
+
         self._ref = ref
         with self:
-            with ui.card().classes().classes(bg_200):
+            # Annoyingly, because of css specificity, we need to manually set
+            # colors via the color and text-color prop, instead of using one of
+            # our preset styles.
+            self.text_chip = (
+                ui.chip(text, selectable=True, selected=False)
+                .props(f'color="{light.primary}"')
+                .props(f'text-color="{light.primary_content}"')
+            )
+            with ui.card().classes().classes(bg_200) as card:
+                self.card = card
+                self.card.bind_visibility_from(self.text_chip, "selected")
                 ui.markdown(ref.content).classes("text-xl")
                 ui.button(
                     "Go to definition",
                     on_click=lambda: ui.navigate.to(self._ref),
-                ).props('color="teal" no-caps')
+                ).props(f'color="{light.primary}" no-caps').props(
+                    f'text-color="{light.primary_content}"'
+                )
