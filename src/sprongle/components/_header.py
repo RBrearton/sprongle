b9568b7ba1f5cc3979da1bcacc8db8835b823dde
Richard Brearton <richardbrearton@gmail.com>
2024-12-04 18:15:06 +0000

    Added a menu for toggling the drawer

--- src/sprongle/components/_header.py
+++ src/sprongle/components/_header.py
@@ -2,7 +2,7 @@
 
 from nicegui import ui
 
-from sprongle.style import bg_primary
+from sprongle.style import bg_primary, header_clickable
 
 from ._dark_mode_toggle import DarkModeToggle
 from ._github_link import GithubLink
@@ -22,6 +22,7 @@ class Header(ui.header):
             add_scroll_padding=True,
         )
         self.classes(bg_primary)
+        self._drawer_ref: ui.drawer | None = None
 
         # Add the contents of the header.
         with self:
@@ -31,10 +32,26 @@ class Header(ui.header):
             ):
                 # Everything here lives on the left.
                 with ui.element().classes(
-                    "flex flex-row items-center justify-start flex-1"
+                    "flex flex-row items-end justify-start flex-1"
                 ):
+                    # Toggle the drawer if we've been given a reference to it.
+                    self.drawer_toggle = (
+                        ui.button(icon="menu")
+                        .classes("text-lg mb-[2px]")
+                        .classes(header_clickable)
+                        .classes("text-white p-2")
+                        .classes("xl:hidden")
+                        .props("flat round")
+                        .on(
+                            "click",
+                            lambda: self._drawer_ref.toggle()
+                            if self._drawer_ref
+                            else None,
+                        )
+                    )
+
                     # Add the title.
-                    ui.markdown(f"#### {title}").classes("py-2")
+                    ui.markdown(f"#### {title}").classes("p-2")
 
                 # Add the central buttons.
                 with ui.element().classes(
@@ -50,3 +67,7 @@ class Header(ui.header):
                 ):
                     DarkModeToggle()
                     GithubLink()
+
+    def set_drawer_ref(self, drawer_ref: ui.drawer) -> None:
+        """Set the reference to the drawer."""
+        self._drawer_ref = drawer_ref
