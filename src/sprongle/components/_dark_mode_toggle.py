a7e8d20b1912e5dbaf8b17261e282d49d9a0747b
Richard Brearton <richardbrearton@gmail.com>
2024-12-04 14:33:53 +0000

    Dark mode is now persistent

--- src/sprongle/components/_dark_mode_toggle.py
+++ src/sprongle/components/_dark_mode_toggle.py
@@ -1,6 +1,6 @@
 """Define the DarkModeToggle component."""
 
-from nicegui import ui
+from nicegui import app, ui
 
 
 class DarkModeToggle(ui.element):
@@ -10,12 +10,14 @@ class DarkModeToggle(ui.element):
         super().__init__()
 
         self.dark_mode = ui.dark_mode()
+        self.dark_mode.bind_value(app.storage.user)
 
         with self.classes("flex flex-row items-center h-full"):
             ui.icon("light_mode").classes("text-2xl")
-            self.switch = ui.switch(on_change=self.dark_mode.toggle).classes(
-                "text-secondary"
-            )
+            self.switch = ui.switch(
+                value=self.dark_mode.value or False,
+                on_change=self.dark_mode.toggle,
+            ).classes("text-secondary")
             self.switch.props["color"] = "white"
             self.switch.props("keep-color")
             ui.icon("dark_mode").classes("text-2xl")
